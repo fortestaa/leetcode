c1ff4a6c59dfce4c38655b10495021dba48ac28b
fortestaa <for_relaxaa@163.com>
2015-10-18 22:09:18 -0700

    Update and rename LongestPalindromicSubstring.java to No5_LongestPalindromicSubstring.java

--- medium/No5_LongestPalindromicSubstring.java
+++ medium/No5_LongestPalindromicSubstring.java
@@ -6,7 +6,7 @@ package medium;
  * Given a string S, find the longest palindromic substring in S.
  * You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
  */
-public class LongestPalindromicSubstring {
+public class No5_LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
         int start = 0, end = 0;
         for (int i = 0; i < s.length(); i++) {
