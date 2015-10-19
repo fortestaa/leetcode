e1434337283aa591fdaa9e92eade9f9947cc77db
fortestaa <for_relaxaa@163.com>
2015-10-18 22:04:51 -0700

    Update and rename Longest_Substring_Without_Repeating_Characters.java to No3_Longest_Substring_Without_Repeating_Characters.java

--- medium/No3_Longest_Substring_Without_Repeating_Characters.java
+++ medium/No3_Longest_Substring_Without_Repeating_Characters.java
@@ -2,7 +2,15 @@ package medium;
 
 import java.util.Arrays;
 
-public class Longest_Substring_Without_Repeating_Characters {
+/*
+3	Longest Substring Without Repeating Characters
+https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+Given a string, find the length of the longest substring without repeating characters. 
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
+For "bbbbb" the longest substring is "b", with the length of 1.
+*/
+public class No3_Longest_Substring_Without_Repeating_Characters {
     public int lengthOfLongestSubstring(String s) {
         int[] charMap = new int[256];
         Arrays.fill(charMap, -1);
