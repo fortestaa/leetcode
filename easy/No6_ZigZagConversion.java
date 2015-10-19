6b6d9f99e7547defe2bce02252f5656b6aea1c05
fortestaa <for_relaxaa@163.com>
2015-10-18 22:27:07 -0700

    Update and rename ZigZagConversion.java to No6_ZigZagConversion.java

--- easy/No6_ZigZagConversion.java
+++ easy/No6_ZigZagConversion.java
@@ -1,6 +1,18 @@
 package easy;
 
-public class ZigZagConversion {
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+(you may want to display this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+*/
+public class No6_ZigZagConversion {
     public String convert(String s, int numRows) {
         StringBuffer[] sb = new StringBuffer[numRows];
         for (int i = 0; i < numRows; i++)   sb[i] = new StringBuffer();
