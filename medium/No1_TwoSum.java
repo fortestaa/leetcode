0c6b21d52a7072eb02064301b05115c549ff40d1
fortestaa <for_relaxaa@163.com>
2015-10-18 21:59:29 -0700

    Update No1_TwoSum.java

--- medium/No1_TwoSum.java
+++ medium/No1_TwoSum.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 1	Two Sum
+ * https://leetcode.com/problems/two-sum/
  * Given an array of integers, find two numbers such that they add up to a
  * specific target number.
  * 
