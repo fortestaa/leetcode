469a20d8b105bbcbb40d07521b867abd36ed9b0d
fortestaa <for_relaxaa@163.com>
2015-10-18 22:52:38 -0700

    Update and rename Remove_Nth_Node_From_End_of_List.java to No19_Remove_Nth_Node_From_End_of_List.java

--- easy/No19_Remove_Nth_Node_From_End_of_List.java
+++ easy/No19_Remove_Nth_Node_From_End_of_List.java
@@ -11,7 +11,7 @@ package easy;
  * After removing the second node from the end, the linked list becomes 1->2->3->5. 
  * Note: Given n will always be valid. Try to do this in one pass.
  */
-public class Remove_Nth_Node_From_End_of_List {
+public class No19_Remove_Nth_Node_From_End_of_List {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode pre = new ListNode(0);
         pre.next = head;
